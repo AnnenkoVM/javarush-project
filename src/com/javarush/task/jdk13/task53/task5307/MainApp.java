bf199f778ea6eb32b64bed3b3a83c47676ae5b3b
Vadim Annenko <vadim.shadow28@gmail.com>
2025-05-25 21:10:15 +0400

    Default code

---
+++ src/com/javarush/task/jdk13/task53/task5307/MainApp.java
@@ -0,0 +1,7 @@
+package com.javarush.task.jdk13.task53.task5307;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Логика для выбора режима работы, вызов соответствующих методов
+    }
+}
---
+++ src/com/javarush/task/jdk13/task53/task5307/Validator.java
@@ -0,0 +1,10 @@
+package com.javarush.task.jdk13.task53.task5307;
+
+public class Validator {
+    public boolean isValidKey(int key, char[] alphabet) {
+        // Проверка ключа
+    }
+    public boolean isFileExists(String filePath) {
+        // Проверка существования файла
+    }
+}
